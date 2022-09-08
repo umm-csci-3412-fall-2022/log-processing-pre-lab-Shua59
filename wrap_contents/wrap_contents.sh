e034940ff87eef2a0c7880a6a44e7086cfabaa2f
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-08 10:40:17 -0500

    Create wrapping of file contents into a destination file

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -2,4 +2,6 @@
 
 textFile=$1 #Stores the content file as an argument
 headerAndFooter=$2 #Stores the files of the headers and the footers as an argument
-fileDestination=$3 #Stores the detination of the wrapped contents as an argument 
+fileDestination=$3 #Stores the detination of the wrapped contents as an argument
+
+cat ${headerAndFooter}_header.html $textFile ${headerAndFooter}_footer.html > $fileDestination
