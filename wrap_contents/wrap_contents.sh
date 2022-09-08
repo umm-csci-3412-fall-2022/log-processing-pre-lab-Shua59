c8389b0b8d051e249d079e0e9ed422c0205cd197
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-08 12:25:09 -0500

    Commit to add a tag

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -3,7 +3,7 @@
 textFile=$1 #Stores the content file as an argument
 headerAndFooter=$2 #Stores the files of the headers and the footers as an argument
 fileDestination=$3 #Stores the detination of the wrapped contents as an argument
-
+ 
 # Use concatnate to put the textFile in between the header and the footer
 # Subsequentally putting the result into the destination file
 cat "${headerAndFooter}_header.html" "$textFile" "${headerAndFooter}_footer.html" > "$fileDestination"
