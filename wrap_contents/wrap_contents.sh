7ff4fa8b89061932fd826e5043050b718ceebab0
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-08 10:45:10 -0500

    Fix notation errors to pass shellcheck tests Add comments to explain code

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -4,4 +4,6 @@ textFile=$1 #Stores the content file as an argument
 headerAndFooter=$2 #Stores the files of the headers and the footers as an argument
 fileDestination=$3 #Stores the detination of the wrapped contents as an argument
 
-cat ${headerAndFooter}_header.html $textFile ${headerAndFooter}_footer.html > $fileDestination
+# Use concatnate to put the textFile in between the header and the footer
+# Subsequentally putting the result into the destination file
+cat "${headerAndFooter}_header.html" "$textFile" "${headerAndFooter}_footer.html" > "$fileDestination"
