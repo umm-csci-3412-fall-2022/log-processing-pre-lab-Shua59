7048001d4ff27e5e9e6ea9fbe967545f4a01295e
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-08 12:14:54 -0500

    Alter answer for middle regex exercise, but still not passing

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+# Use the stream editor to filter and transform the given texts
+
 ## Regex Exercise 0
-#First, make a outfile for regex exercise 0.
-touch r0_output.txt
-# use stream editor to filter and tranform text 
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 ## Regex Exercise 1
-# use stream editor to filter and tranform text
-sed -n -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-zA-Z]+)/1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
+sed -ni -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-zA-Z]+)/1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
 
 ## Regex Exercise 2
-# use stream editor to filter and tranform text 
 sed -E 's/\* sandwich with ([a-zA-Z.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
 
