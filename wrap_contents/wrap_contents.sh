9d33698ece5d30e6695bf53a4f766fd0b49d355b
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-08 10:32:52 -0500

    Add argument names to files that will be used later

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+textFile=$1 #Stores the content file as an argument
+headerAndFooter=$2 #Stores the files of the headers and the footers as an argument
+fileDestination=$3 #Stores the detination of the wrapped contents as an argument 
